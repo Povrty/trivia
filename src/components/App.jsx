19568e42c502fb9b1884f9c648b57c7dc640688f
Povrty <43181377+Povrty@users.noreply.github.com>
2021-02-04 17:26:56 -0800

    Goal 1 completed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,16 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+function Question(props){
+  return <p>{props.question}</p>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  console.log(data[0].question.text);
+  let question=0;
+  return (<div className="app">Trivia!
+    <Question question={data[question].question.text}/>
+  </div> );
 }
 
 export default App;
