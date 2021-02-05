452cf8900eb09b52d9ffec06e914b978ce6519ec
Povrty <43181377+Povrty@users.noreply.github.com>
2021-02-04 17:57:11 -0800

    Goal 1 & 2 finished.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,16 @@ function Question(props){
   return <p>{props.question}</p>;
 }
 
+function NextQuestion() {
+  return <button>Next Question</button>;
+}
+
 function App() {
   console.log(data[0].question.text);
   let question=0;
   return (<div className="app">Trivia!
     <Question question={data[question].question.text}/>
+    <NextQuestion />
   </div> );
 }
 
