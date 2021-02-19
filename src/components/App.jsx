8164a029f989275dbd563c03052873e3b82eccdc
Povrty <43181377+Povrty@users.noreply.github.com>
2021-02-18 17:58:40 -0800

    Goal 1 Day 2 Completed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,16 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function Question(props){
-  return <p>{props.question}</p>;
+  console.log(data[0].question.choices);
+  let answers = props.answers;
+  let question = props.question;
+  let newAnswers = answers.map(answer => <Answer text={answer}/>);
+  return (<div>
+    <p>{question}</p>
+    <div>
+      {newAnswers}
+    </div>
+    </div>);
 }
 
 function NextQuestion() {
@@ -11,12 +20,23 @@ function NextQuestion() {
 }
 
 function App() {
-  console.log(data[0].question.text);
-  let question=0;
+  let question = 0;
+  let answers = data[question].question.choices;
+  let [isAnswered, setIsAnswered] = useState(false);
+  const buttonClicked = setIsAnswered(true);
   return (<div className="app">Trivia!
-    <Question question={data[question].question.text}/>
+    <Question question={data[question].question.text} answers={answers}/>
+    <div>
+      <button>Show the correct answer</button>
+    </div>
     <NextQuestion />
   </div> );
+}
+
+function Answer(props) {
+  return(
+    <p>{props.text}</p>
+  );
 }
 
 export default App;
