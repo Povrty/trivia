fc3ea10acb461b92f3400c90c4b9378af0a9df26
Povrty <43181377+Povrty@users.noreply.github.com>
2021-05-10 15:29:52 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "/src/css/App.css";
-import App from "/src/components/App";
+import "./css/App.css";
+import App from "./components/App";
 
 // render the App in to the DOM inside the element with id "root"
 ReactDOM.render(<App />, document.getElementById("root"));
